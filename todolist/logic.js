1bb2c25e867cf4b04b1864d49cb5ad33afad2dfd
lerwing <adminosb@gmail.com>
2022-05-11 22:06:23 +0300

    первый реф логики

--- todolist/logic.js
+++ todolist/logic.js
@@ -1,4 +1,7 @@
-;(function todoList() {
+;"use strict";
+
+(function todoList() {
+const INACTIVE_INDEX = -1;
 // метод получает значение из класса и записывает в переменную
 // tip = Цели(категории) task = Задачи (привязаны к целям)
 let tipTxt = document.querySelector('.tip'),
@@ -8,78 +11,84 @@ let tipTxt = document.querySelector('.tip'),
     taskBut = document.querySelector('.taskAdd'),
     task = document.querySelector('.taskLi')
 ;
-//let tipTemp = [];
-//let taskTemp = [];
+
 //Создаем двумерный массив для целей и задач в виде объекта из двух массивов которые будут содрежать объекты
 let data = {
     'tip': [],
     'task': [],
-    tipActivIndex: -1
+    tipActivIndex: INACTIVE_INDEX
 };
 
-const output =() =>{
-    //вывод задач
+//Элементы верстки
+//Цели
+const getHtmlTip = (item, i) => {
+    return `
+    <li id="tip_${i}" ${item.select ? 'class="select"' : ''}>
+    <span class="dell" id="tipD_${i}">X</span>${item.text}
+    </li>
+    `
+};
+
+//Задачи
+const getHtmlTask = (item, i) => {
+    return `
+    <li id="tas_${i}" ${item.chek ? 'class="chek"' : ''}>
+    <span class="dell" id="tasD_${i}">X</span>${item.text}
+    </li>
+    `
+};
+
+//метод вывода
+const output = () => {
     let outputTip = '';
     //если массив целей пуст выводи пустоту
-    if (data.tip.length === 0){
+    if (!data.tip.length) {
         tip.innerHTML = '';
-        data.tipActivIndex = -1;
+        data.tipActivIndex = INACTIVE_INDEX;
         //сохраняем данные в локал сторедж
         localStorage.setItem('data', JSON.stringify(data));
     };
     //сканируем массив целей и выводим строку li для каждого элемента массива
-    data.tip.forEach(function(item, i){
-        outputTip += `
-        <li id="tip_${i}" ${item.select ? 'class="select"' : ''}>
-        <span class="dell" id="tipD_${i}">X</span>${item.text}
-        </li>
-        `;
-        
+    data.tip.forEach((item, i) => {
+        outputTip += getHtmlTip(item, i);
         //console.log(i)
     });
     tip.innerHTML = outputTip;
     //То же самое для задач, задачи привязаны к целям
-    let outputTask = '';
-    // Проверяем есть ли задачи для выбранной цели. Для этого преобразуем набор элементов tip в простой массив и проверяем
-    if (data.task.map(el => el.tip).includes(data.tipActivIndex) === false){
+    // Проверяем есть ли задачи для выбранной цели. Фильтруем массив задач по активному индексу и проверяем его длинну
+    if (!data.task.filter(el => el.tip === data.tipActivIndex).length) {
         task.innerHTML = '';
-    } else{
-        data.task.forEach(function(item, i){
-            if (data.tipActivIndex == item.tip){
-                outputTask +=`
-            <li id="tas_${i}" ${item.chek ? 'class="chek"' : ''}>
-            <span class="dell" id="tasD_${i}">X</span>${item.text}
-            </li>
-            `;
-            
-            //console.log(data.tipActivIndex);
-            //console.log(item.tip);
-            };
-        });
+    } else {
+        const outputTask = data.task.reduce((acc, item, i) => {
+            const temp = data.tipActivIndex === item.tip ? getHtmlTask(item, i) : '';
+            return  `${acc}${temp}`;
+        }, '' );
         task.innerHTML = outputTask;
     };
 };
 
-
 // Стрелочные функци обработчиков событий нажатий
-//метод обработки нажатия кнопки получаем введенное название цели
-const addTip = () =>{
+// метод обработки нажатия кнопки получаем введенное название цели
+const addTip = () => {
     let newTip = {
         text: tipTxt.value,
         select: false
     };
     //Записываем новую цель в главный массив
-    if (tipTxt.value !== '') data.tip.push(newTip);
+    if (tipTxt.value) {
+        data.tip.push(newTip);
+    };
     //сохраняем данные в локал сторедж
     localStorage.setItem('data', JSON.stringify(data));
     //очищаем поле воода
     tipTxt.value = '';
     output();
 };
-//метод обработки нажатия на кнопку добавить задачу
-const addTask = () =>{
-    if (data.tipActivIndex === -1){
-        alert("Для ввода задчи выберите цель")
+
+// метод обработки нажатия на кнопку добавить задачу
+const addTask = () => {
+    if (data.tipActivIndex === INACTIVE_INDEX) {
+        alert("Для ввода задачи выберите цель")
     } else {
         let newTask = {
             tip: data.tipActivIndex,
@@ -87,7 +96,7 @@ const addTask = () =>{
             chek: false
         };
         //console.log(newTask);
-        if (taskTxt.value !== '') data.task.push(newTask);
+        if (taskTxt.value) data.task.push(newTask);
         //сохраняем данные в локал сторедж
         localStorage.setItem('data', JSON.stringify(data));
         //очищаем поле воода
@@ -95,54 +104,49 @@ const addTask = () =>{
         output();
     };
 };
-//Метод выбора и удаление целей
-const selectDellTip = (event) =>{
+
+// Метод выбора и удаление целей
+const selectDellTip = (event) => {
     //получаем ID элемента LI
     let tipLi=event.target.getAttribute('id');
     console.log(tipLi);
     //Проверка на что нажали удалить или выделить
-    if (tipLi.search("tip_") !== -1){
+    if (tipLi.search("tip_") !== INACTIVE_INDEX) {
         //отбрасываем текстовую часть tip_
         tipLi = tipLi.slice(4);
         //преобразуем в число
         tipLi = Number.parseInt(tipLi);
         //устанавливаем значение false всем целям
-        for(let i =0; i < data.tip.length; i++){
-            data.tip[i].select = false;
-        };
+        data.tip.forEach((item) => {
+            item.select = false;
+        });
         //устанавливаем значение true выбранной цели
         data.tip[tipLi].select = true;
         data.tipActivIndex = tipLi;
-    } else{
-        //отбрасываем текстовую часть tipD_
+    } else {
+        // отбрасываем текстовую часть tipD_
         tipLi = tipLi.slice(5);
-        //преобразуем в число
+        // преобразуем в число
         tipLi = Number.parseInt(tipLi);
-        //Удаляем элемент из массива
+        // Удаляем элемент из массива
         data.tip.splice(tipLi, 1);
-        //удаляем связанные задачи
-        for (let i = 0; i < data.task.length; i++){
-            if (data.task[i].tip === tipLi){
-                data.task.splice(i, 1);
-                //так как при удалении эдемента массив сдвигается то смещаем и индекс цыкла
-                i = i - 1;
-            };
-        };
+        // удаляем связанные задачи
+        data.task = data.task.filter((item) => item.tip !== tipLi);
     };
-    //console.log(tipLi);
-    //сохраняем данные в локал сторедж
+    // сохраняем данные в локал сторедж
     localStorage.setItem('data', JSON.stringify(data));
     console.log(JSON.stringify(data));
     // Выводим на экран, Ура!!! заработало!!!
     output()
 };
+
 //Метод выбора и удаление задач
 const selectDellTask = (event) =>{
     //получаем ID элемента LI
     let taskLi = event.target.getAttribute('id');
     console.log(taskLi);
     //Проверка на что нажали удалить или пометить
-    if (taskLi.search("tas_") !== -1){
+    if (taskLi.search("tas_") !== INACTIVE_INDEX){
         //отбрасываем текстовую часть tas_
         taskLi = taskLi.slice(4);
         //преобразуем в число
@@ -166,11 +170,13 @@ const selectDellTask = (event) =>{
     
 };
 
+//Ход программы
+
 //проверяем есть ли что то в БД и если есть то заполниема массив data и выводим на экран
 if(localStorage.getItem('data')){
     data = JSON.parse(localStorage.getItem('data'));
     output();
-}
+};
 //кнопка добавить дель
 tipBut.addEventListener('click', addTip);
 //кнопка добавить задачу
@@ -179,7 +185,5 @@ taskBut.addEventListener('click', addTask);
 tip.addEventListener('click', selectDellTip);
 //помечаем или удаляем задачи
 task.addEventListener('click', selectDellTask);
-//Вывод данных
-
 
 }());
